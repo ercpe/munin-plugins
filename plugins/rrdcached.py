08513e59b74b4c94d00402261d446c99f659d174
Johann Schmitz <johann@j-schmitz.net>
2016-05-29 09:17:00 +0200

    Improved labels of rrdcached plugin

--- plugins/rrdcached.py
+++ plugins/rrdcached.py
@@ -13,26 +13,26 @@ if len(sys.argv) > 1 and sys.argv[1] == 'config':
 graph_category rrdcached
 QueueLength.label Queue length
 QueueLength.info Number of nodes currently enqueued in the update queue.
-UpdatesReceived.label UpdatesReceived
+UpdatesReceived.label Update commands received
 UpdatesReceived.type DERIVE
 UpdatesReceived.info Number of UPDATE commands received.
-FlushesReceived.label FlushesReceived
+FlushesReceived.label Flush command received
 FlushesReceived.type DERIVE
 FlushesReceived.info Number of FLUSH commands received.
-UpdatesWritten.label UpdatesWritten
+UpdatesWritten.label Updates written
 UpdatesWritten.type DERIVE
 UpdatesWritten.info Total number of updates, i. e. calls to rrd_update_r, since the daemon was started.
-DataSetsWritten.label DataSetsWritten
+DataSetsWritten.label DataSets written
 DataSetsWritten.type DERIVE
 DataSetsWritten.info Total number of "data sets" written to disk since the daemon was started.
-TreeNodesNumber.label TreeNodesNumber
+TreeNodesNumber.label Number of tree nodes
 TreeNodesNumber.info Number of nodes in the cache.
-TreeDepth.label TreeDepth
+TreeDepth.label Tree depth
 TreeDepth.info Depth of the tree used for fast key lookup.
-JournalBytes.label JournalBytes
+JournalBytes.label Total journal bytes written
 JournalBytes.type DERIVE
 JournalBytes.info Total number of bytes written to the journal since startup.
-JournalRotate.label JournalRotate
+JournalRotate.label Journal rotated
 JournalRotate.type DERIVE
 JournalRotate.info Number of times the journal has been rotated since startup."""
 else:
